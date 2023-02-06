d1121a22a66d15bb0242f77b79e3828be640efb6
berger <robertberger.dev@gmail.com>
2023-02-06 14:36:50 -0500

    reverted back to first example code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const heading = React.createElement('h1', null, 'Hello World!');
+// const heading = React.createElement('h1', null, 'Hello World!');
 
-const l1 = React.createElement('li', null, 'Bananas');
-const l2 = React.createElement('li', null, 'Milk');
-const l3 = React.createElement('li', null, 'Bread');
+// const l1 = React.createElement('li', null, 'Bananas');
+// const l2 = React.createElement('li', null, 'Milk');
+// const l3 = React.createElement('li', null, 'Bread');
 
-const groceryList = React.createElement('ol', null, [l1, l2, l3]);
+// const groceryList = React.createElement('ol', null, [l1, l2, l3]);
 
-ReactDOM.render([heading, groceryList], document.getElementById('root'));
+// ReactDOM.render([heading, groceryList], document.getElementById('root'));
 
-// const title = React.createElement('h1', null, 'My First React App');
-// ReactDOM.render(title, document.getElementById('root'));
+const title = React.createElement('h1', null, 'My First React App');
+ReactDOM.render(title, document.getElementById('root'));
 
 
 // import React from 'react';
